69d04012e1db29aac658b4ecb4f64ee51bc3dc67
Daria Dmitrochenko <dariadmitrochenko@Evgeny.lan>
2024-05-29 06:17:18 -0400

    Frontend finishing touches

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import { useState } from "react";
 import api from "../api";
-import "../styles/Form.css"
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
-function Form ({ route, method }) {
+function Form({ route, method }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
@@ -17,19 +18,18 @@ function Form ({ route, method }) {
     e.preventDefault();
 
     try {
-        const res = await api.post(route, { username, password })
-        if (method === "login") {
-            localStorage.setItem(ACCESS_TOKEN, res.data.access);
-            localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-            navigate("/")
-        } else {
-            navigate("/login")
-        }
-
+      const res = await api.post(route, { username, password });
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
     } catch (error) {
-        alert(error)
+      alert(error);
     } finally {
-        setLoading(false)
+      setLoading(false);
     }
   };
 
@@ -50,10 +50,12 @@ function Form ({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+      {loading && <LoadingIndicator />}
       <button className="form-button" type="submit">
         {name}
       </button>
     </form>
   );
 }
-export default Form
+
+export default Form;
